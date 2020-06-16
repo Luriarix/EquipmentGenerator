694894f3a2174148db34c5c74dae9769ed3dd75e
Luriarix <ionitaandrei_lucian@yahoo.ro>
2020-06-16 23:51:47 +0100

    Updated ReadMe

--- EquipmentGeneratorWPF/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ EquipmentGeneratorWPF/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -93,7 +93,7 @@ namespace EquipmentGeneratorWPF {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/EquipmentGeneratorWPF;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/EquipmentGeneratorWPF;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- README.md
+++ README.md
@@ -28,9 +28,14 @@
 
 ### Screenshot of the Kanban board
 #### Sprint 1
+##### Start
 <img src="Sprint 1 start.png" width = "242"/>
+
+##### Middle
 <img src="Sprint 1 review.png" width = "300"/>
-<img src="Sprint 1 review.png" width = "300"/>
+
+#### End of Sprint 1 - Start of Sprint 2
+<img src="Sprint 1 end - Sprint 2 Start.png" width = "300"/>
 
 #### Sprint N
 <img src= width = "300"/>
